ade67f198334c7c095a21affa778332607b8031b
xsite <xsite@xsite-work1>
2020-10-13 16:17:12 +0200

    fixed typo in start server script

--- Start_Server.sh
+++ Start_Server.sh
@@ -2,4 +2,4 @@
 UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
 chmod +x "$UE4_PROJECT_ROOT/xsite_ue_example/Binaries/Linux/xsite_ue_example"
-"$UE4_PROJECT_ROOT/xsite_ue_example/Binaries/Linux/xsite_ue_example" xsite_ue_example /Game/NewMap?listen -game -lanplay
+"$UE4_PROJECT_ROOT/xsite_ue_example/Binaries/Linux/xsite_ue_example" xsite_ue_example NewMap?listen -game -lanplay
